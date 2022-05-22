6a4de8e9c5b14d84cf8c093fd120da32142111ab
jprk <yennarine@163.com>
2022-05-22 20:13:38 +0800

    avx support

---
+++ src/avx/udl_vector_math.c
@@ -0,0 +1,32 @@
+#include <udl.h>
+#include <immintrin.h>
+
+#define __VEC_COUNT 8
+
+m_t mac_mmm(m_t * a, m_t * b, uint64_t size) {
+  m_t r = 0;
+  float i0[__VEC_COUNT] __attribute__((aligned (32)));
+  float i1[__VEC_COUNT] __attribute__((aligned (32)));
+  float  o[__VEC_COUNT] __attribute__((aligned (32)));
+  uint64_t idx = 0;
+  for (uint64_t i=0;i<size;i+=__VEC_COUNT) {
+    for (uint64_t j=0;j<__VEC_COUNT;j++) {
+      if (idx < size) {
+        i0[j] = a[idx];
+        i1[j] = b[idx];
+      } else {
+        i0[j] = 0;
+        i1[j] = 0;
+      }
+      idx++;
+    }
+    __m256 v0 = _mm256_load_ps(i0);
+    __m256 v1 = _mm256_load_ps(i1);
+    __m256 v  = _mm256_mul_ps(v0, v1);
+    _mm256_store_ps(o, v);
+    for (uint64_t j=0;j<__VEC_COUNT;j++) {
+      r = add_mmm(r, o[j]);
+    }
+  }
+  return r;
+}
